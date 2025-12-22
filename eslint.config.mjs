0522acf565054d712244ccc652c86851b3a8b002
TolyaPoznyakov <poznjakov373@gmail.com>
2025-12-22 20:58:05 +0200

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,13 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'indent': ['error', 2], // вкладеність 2 пробіли
+    'object-curly-spacing': ['error', 'always'], // відступи в import
+    'no-trailing-spaces': 'error', // помилка якщо пробіл в кінці рядка
+    'semi': ['error', 'never'], // без крапок з комою
+    'comma-dangle': ['error', 'never'], // без коми після останньої властивості
+    'arrow-spacing': ['error', { 'before': true, 'after': true }] // пробіли навколо =>
+  }
+})
