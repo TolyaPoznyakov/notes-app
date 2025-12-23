aee92347bfd2b5238c12a86e2907e68ce74bb5bf
TolyaPoznyakov <poznjakov373@gmail.com>
2025-12-23 16:57:07 +0200

    added a notes page and a notes list

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,22 +1,40 @@
 <template>
-<div>
-  <div
-      v-for="note in notes"
-      :key="note._id"
-  >
-    <h3> {{ note.title }}</h3>
-    <p> {{ note.text }}</p>
+  <div>
+    <div class="grid place-items-center mt-[10px] ">
+      <Card
+          class="m-5 w-md h-64 p-4"
+      >
+        <Input v-model="title" placeholder="Title"/>
+        <Textarea v-model="text" placeholder="Text"/>
+        <Button class="w-30" variant="outline" :disabled="loading" @click="createNote">Add note</Button>
+      </Card>
+    </div>
+    <div class="grid grid-cols-4">
+      <Card
+          v-for="note in notes"
+          :key="note._id"
+          class="w-xs m-5 p-5"
+      >
+        <h3 class="mb-3 h-1"> {{ note.title }}</h3>
+        <hr>
+        <p> {{ note.text }}</p>
+      </Card>
+    </div>
   </div>
-  <Button variant="outline" :disabled="loading" @click="createNote">Add note</Button>
-</div>
 </template>
 
 <script setup>
 import { useApiRequest } from "~/composables/apiRequest.js"
 import routes from "~/const/routes.js"
 import { Button } from "~/components/ui/button/index.js"
+import { Input } from "~/components/ui/input/index.js"
+import { Textarea } from "~/components/ui/textarea/index.js"
+import { Card } from "~/components/ui/card/index.js"
+
 
 const notes = ref([])
+const title = ref('')
+const text = ref('')
 const loading = ref(false)
 
 const fetchNotes = async () => {
@@ -25,15 +43,19 @@ const fetchNotes = async () => {
 }
 
 const createNote = async () => {
+  if (!title.value || !text.value) return
+
   loading.value = true
   const res = await useApiRequest(routes.notes.list, {
     method: 'POST',
     body: {
-      title: 'Example title',
-      text: 'Lorem Ipsum test tsetes qwqwfqfqf'
+      title: title.value,
+      text: text.value
     }
   })
   notes.value.push(res.data.value)
+  title.value = ''
+  text.value = ''
   loading.value = false
 }
 
